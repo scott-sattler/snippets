ed952c2f64e09e0ad52a1461bc037daf9b2b7e00
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-05-24 20:19:14 -0500

    rearranged

--- rps.py
+++ rps.py
@@ -1,7 +1,7 @@
 # code on GitHub at ./snippets/rps.py
 from random import choice; one_line_rps_with_memory_and_input_check_ver_02 = \
-[scr := [], evl_inp_gme := lambda usr_inp: range(int(usr_inp)) if usr_inp.isdigit() else evl_inp_gme(input("Number of g\
-ames to play? ")), [print([f"Tie: {usr.capitalize()} vs {cho}", scr.append(0)][0] if (cho := choice(rps := (pro := "Cho\
+[evl_inp_gme := lambda usr_inp: range(int(usr_inp)) if usr_inp.isdigit() else evl_inp_gme(input("Number of games to pla\
+y? ")), scr := [], [print([f"Tie: {usr.capitalize()} vs {cho}", scr.append(0)][0] if (cho := choice(rps := (pro := "Cho\
 ose: Rock Paper Scissors  ").split()[1:])).lower() == (usr := (evl_inp_cho := lambda usr_inp: usr_inp if usr_inp.
 capitalize() in rps else evl_inp_cho(input(pro)))('-e')).lower() else ([f"Win {usr.capitalize()} vs {cho}", scr.append
 (1)][0] if ({k: ['s', 'k', 'r'][i] for i, k in enumerate([i[-1] for i in rps])})[usr[-1]] == cho[-1] else [f"Lose \
