1c2b5313d72a8aabfd1845c23e9d5e03b5ae8466
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2023-08-15 13:08:07 -0500

    some clarity; omfglol

---
+++ rps_working.py
@@ -0,0 +1,28 @@
+# # code on GitHub at ./snippets/rps.py
+# from random import choice; one_line_rps_with_memory_and_input_check_ver_02 = \
+# [evl_inp_gme := lambda usr_inp: range(int(usr_inp)) if usr_inp.isdigit() else evl_inp_gme(input("Number of games to pla\
+# y? ")), scr := [], [print([f"Tie: {usr.capitalize()} vs {cho}", scr.append(0)][0] if (cho := choice(rps := (pro := "Cho\
+# ose: Rock Paper Scissors  ").split()[1:])).lower() == (usr := (evl_inp_cho := lambda usr_inp: usr_inp if usr_inp.
+# capitalize() in rps else evl_inp_cho(input(pro)))('-e')).lower() else ([f"Win {usr.capitalize()} vs {cho}", scr.append
+# (1)][0] if ({k: ['s', 'k', 'r'][i] for i, k in enumerate([i[-1] for i in rps])})[usr[-1]] == cho[-1] else [f"Lose \
+# {usr.capitalize()} vs {cho}", scr.append(0)][0])) for i in evl_inp_gme('foobar')], print(f'Game Over: {sum(scr)} wins i\
+# n {len(scr)} games.')]  # noqa
+
+
+# code on GitHub at ./snippets/rps.py
+from random import choice; one_line_rps_nightmare_with_memory_and_input_check_v03 = [
+    get_inp := lambda usr_inp='foobar': range(int(usr_inp)) if usr_inp.isdigit() else get_inp(input("Number of games to play? ")),
+    display := lambda xyz: print(f"{'WinLossTie'[xyz[0]:xyz[1]]}: {usr_inp.capitalize()} vs {get_cpu}"),
+    games := [[display((7,256)), 0][1]
+            if (get_cpu := choice(rps := (prompt := "Choose: Rock Paper Scissors  ").split()[1:])).lower() ==
+               (usr_inp := (evl_play := lambda usr_inp: usr_inp
+                if usr_inp.capitalize() in rps
+                else evl_play(input(prompt)))('-e')).lower()
+            else ([display((-0,3)), 1][1]
+                if ({k: ['s', 'k', 'r'][i] for i, k in enumerate([i[-1] for i in rps])})[usr_inp[-1]] == get_cpu[-1]
+                else [display((3,7)), 0][1])
+     for i in get_inp()],
+    print(f'Game Over: {sum(games)} wins in {len(games)} games.')
+]  # noqa
+print(one_line_rps_nightmare_with_memory_and_input_check_v03)
+print(games)
