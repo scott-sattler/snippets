f6d7fca22c62462fe4eb30e6a7711b78a7282a2a
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2023-08-15 13:09:42 -0500

    v2 copy

---
+++ rps_v2.py
@@ -0,0 +1,9 @@
+# code on GitHub at ./snippets/rps.py
+from random import choice; one_line_rps_with_memory_and_input_check_ver_02 = \
+[evl_inp_gme := lambda usr_inp: range(int(usr_inp)) if usr_inp.isdigit() else evl_inp_gme(input("Number of games to pla\
+y? ")), scr := [], [print([f"Tie: {usr.capitalize()} vs {cho}", scr.append(0)][0] if (cho := choice(rps := (pro := "Cho\
+ose: Rock Paper Scissors  ").split()[1:])).lower() == (usr := (evl_inp_cho := lambda usr_inp: usr_inp if usr_inp.
+capitalize() in rps else evl_inp_cho(input(pro)))('-e')).lower() else ([f"Win {usr.capitalize()} vs {cho}", scr.append
+(1)][0] if ({k: ['s', 'k', 'r'][i] for i, k in enumerate([i[-1] for i in rps])})[usr[-1]] == cho[-1] else [f"Lose \
+{usr.capitalize()} vs {cho}", scr.append(0)][0])) for i in evl_inp_gme('foobar')], print(f'Game Over: {sum(scr)} wins i\
+n {len(scr)} games.')]  # noqa
