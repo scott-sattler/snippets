0cac9eab6d1f02f3347603416e4af3aa40280545
Scott Sattler <scott.sattler.dev@gmail.com>
2024-09-14 18:07:38 -0500

    fixes

--- colorizer.py
+++ colorizer.py
@@ -1,7 +1,7 @@
 
 class S:
-    # backgrounds '__;__;4?'
     # foregrounds '__;3?;__'
+    # backgrounds '__;__;4?'
     BLACK = 0
     RED = 1
     GREEN = 2
@@ -10,10 +10,11 @@ class S:
     MAGENTA = 5
     CYAN = 6
     GREY = 7
+    DEFAULT = 9
 
-    def __init__(self, background, foreground):
-        self.bg = background
+    def __init__(self, foreground=9, background=9):
         self.fg = foreground
+        self.bg = background
 
     @property
     def c(self):
@@ -21,8 +22,8 @@ class S:
 
 
 if __name__ == '__main__':
-    # for i in range(8):
-    #     for j in range(8):
+    # for i in range(9):
+    #     for j in range(9):
     #         text = 'foo'
     #         print(f'\x1b[00;3{i};4{j}m' + str(text) + '\x1b[0m', f'\ti:{i}; j:{j}')
     #     print()
@@ -31,9 +32,12 @@ if __name__ == '__main__':
 
 
     class Style: RED_BLACK = '6;30;41'; RED = '0;33;31'; YELLOW_BLACK = '5;30;43'  # noqa
-    cprint = lambda txt='', code='': print(f'\x1b[{code}m' + str(txt) + '\x1b[0m')  # noqa
+    cprint = lambda txt='', code='', sep=' ', end='\n': print(f'\x1b[{code}m' + str(txt) + '\x1b[0m', sep=sep, end=end)  # noqa
     # print(ctxt('PASSED ARGS'))
 
 
     # print(ctxt('COLORS', S(S.RED, S.BLACK).c))
-    cprint('COLORS', S(S.RED, S.BLACK).c)
+    # cprint('COLORS', S(S.RED, S.BLACK).c)
+
+    # todo: arbitrary number of text arguments
+    cprint('FOOD', S(background=S.BLACK).c)
